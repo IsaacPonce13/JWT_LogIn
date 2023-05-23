dc5096e1981697af43a003c33b5db16c7f203f9c
IsaacPonce13 <111556869+IsaacPonce13@users.noreply.github.com>
2023-05-22 20:41:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const router = require('./routes/router.js');
+app.use('/api', router);
+
+app.listen(PORT, () => console.log(`Server escuchando en el puerto ${PORT}`));
